25947b3033a8e3b36b98f422aaf86d7e7d57a505
Eric Burlingame <eric@Erics-MacBook-Pro.local>
2015-06-28 10:18:30 -0700

    Attempt to fix usb pro resetting issue

--- utils/reset_usb_pro.py
+++ utils/reset_usb_pro.py
@@ -6,5 +6,30 @@ __author__ = 'eric'
 
 import subprocess
 import os
+import re
+
+
+ftdi_id = "0403:6001"
+
+p = subprocess.Popen(['lsusb', '-d', ftdi_id], stdout=subprocess.PIPE, stderr=subprocess.PIPE)
+out, err = p.communicate()
+
+if err != "":
+    print "Could not find DMX USB Pro, Error: %s" % err
+
+out = "Bus 001 Device 035: ID:0403:6001 Future Tech"
+pattern = "Bus (\d+) Device (\d+): ID:(\d+):(\d+) (.+)"
+match = re.match(pattern, out)
+if match:
+    groups = match.groups()
+    bus = groups[0]
+    port = groups[1]
+    addr = "/dev/bus/usb/%s/%s" % (bus, port)
+    p = subprocess.Popen(['sudo', '~/./usbreset', addr], stdout=subprocess.PIPE, stderr=subprocess.PIPE)
+    out, err = p.communicate()
+    print out
+else:
+    "Output did not match expected format"
+
 
 
